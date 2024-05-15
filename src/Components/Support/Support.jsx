744c691e8a091aa6ea1dc77c24aa845b7fb0af09
Samms206 <sh2069936@gmail.com>
2024-05-15 22:18:09 +0700

    Added-SupportSection

--- src/Components/Support/Support.jsx
+++ src/Components/Support/Support.jsx
@@ -2,7 +2,40 @@ import React from 'react'
 
 const Support = () => {
   return (
-    <div>
+    <div className='support container section'>
+      <div className="sectionContainer">
+
+        <div className="tittlesDiv">
+          <small>Travel Support</small>
+          <h2>Plan your trip with confidence</h2>
+          <p>Find help with booking and travel plans, see what to expect along the journey!</p>
+        </div>
+      </div>
+
+      <div className="infoDiv grid">
+        <div className="textDiv grid">
+
+          <div className="singleInfo">
+            <span className='number'>01</span>
+            <h4>Travel requirement for Dubai</h4>
+            <p>Find help with booking and travel plans, see what to expect along the journey!</p>
+          </div>
+          <div className="singleInfo">
+            <span className='number'>02</span>
+            <h4>Chaueffeur services at your arrival</h4>
+            <p>Find help with booking and travel plans, see what to expect along the journey!</p>
+          </div>
+          <div className="singleInfo">
+            <span className='number'>03</span>
+            <h4>Multi risk travel insurance</h4>
+            <p>Find help with booking and travel plans, see what to expect along the journey!</p>
+          </div>
+        </div>
+
+        <div className="imgDiv">
+          <img src="" alt="" />
+        </div>
+      </div>
       
     </div>
   )

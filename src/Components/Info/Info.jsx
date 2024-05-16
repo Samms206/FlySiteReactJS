93031788ae2984934d85a5a23763ba622f4a8944
Samms206 <sh2069936@gmail.com>
2024-05-16 21:13:30 +0700

    updated-InfoSupport

--- src/Components/Info/Info.jsx
+++ src/Components/Info/Info.jsx
@@ -7,29 +7,29 @@ const Info = () => {
   return (
     <div className="info section">
 
-      <div className="infoContainer container">
+      <div className="infoContainer flex container">
         <div className="titleDiv flex">
           <h2>Travel to make memories all arround the world</h2>
           <button className="btn">View All</button>
         </div>
 
-        <div className="cardDiv grid">
-          <div className="singleCard grid">
+        <div className="cardDiv flex">
+          <div className="singleCard flex">
             <div className="iconDiv flex">
               <RxCalendar className='icon' />
             </div>
             <span className="cardTittle">Book & Relax</span>
             <p>You can also call air lines from your phone and book online</p>
           </div>
-          <div className="singleCard grid">
-            <div className="iconDiv flex">
+          <div className="singleCard flex">
+            <div className="iconDiv flex colorOne">
               <BsShieldCheck className='icon' />
             </div>
             <span className="cardTittle">Smart Checklist</span>
             <p>You can also call air lines from your phone and book online</p>
           </div>
-          <div className="singleCard grid">
-            <div className="iconDiv flex">
+          <div className="singleCard flex">
+            <div className="iconDiv flex colorTwo">
               <BsBookmarkCheck className='icon' />
             </div>
             <span className="cardTittle">Save More</span>

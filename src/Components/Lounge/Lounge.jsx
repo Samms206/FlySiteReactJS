cbbf214813020345267a0dd27d1152f4997ce2f5
Samms206 <sh2069936@gmail.com>
2024-05-17 07:19:00 +0700

    updated-LongueSection

--- src/Components/Lounge/Lounge.jsx
+++ src/Components/Lounge/Lounge.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from "react";
 
 const Lounge = () => {
   return (
-    <div>
-      
+    <div className="longue container section">
+      <div className="sectionContainer grid">
+        <div className="wrapperGrid">
+          <div className="textDiv">
+            <h2>Unaccompanied Minor Lounge</h2>
+          </div>
+          <div className="grids grid">
+            <div className="singleGrid">
+              <span className="gridTitle">Help through the airpot</span>
+              <p>
+                You can also call airlines from your phone and book a flight
+                ticket to one of your favorite destinations.
+              </p>
+            </div>
+            <div className="singleGrid">
+              <span className="gridTitle">Priority Boarding</span>
+              <p>
+                You can also call airlines from your phone and book a flight
+                ticket to one of your favorite destinations.
+              </p>
+            </div>
+            <div className="singleGrid">
+              <span className="gridTitle">Care on the flight</span>
+              <p>
+                You can also call airlines from your phone and book a flight
+                ticket to one of your favorite destinations.
+              </p>
+            </div>
+            <div className="singleGrid">
+              <span className="gridTitle">Care on the flight</span>
+              <p>
+                You can also call airlines from your phone and book a flight
+                ticket to one of your favorite destinations.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="imgDiv">
+          <img src="./src/assets/staff.png" alt="staff" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Lounge
+export default Lounge;

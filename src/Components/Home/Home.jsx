ae04c6f7e1203d60211898cc2fb24c1751d7f5f4
Samms206 <sh2069936@gmail.com>
2024-05-19 22:41:24 +0700

    added-animation

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+// Import AOS
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 const Home = () => {
+  //useEffect to animation
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, [])
+
+
   return (
     <div className='home flex container'>
 
       <div className="mainText">
-        <h1>Create Ever-lasting Memories With Us</h1>
+        <h1 data-aos="fade-up" data-aos-duration="2000">Create Ever-lasting Memories With Us</h1>
       </div>
 
-      <div className="homeImages flex">
+      <div data-aos="fade-up" data-aos-duration="2000" className="homeImages flex">
         <div className="videoDiv">
           <video src="./src/assets/video.mp4" autoPlay loop muted className="video"></video>
         </div>

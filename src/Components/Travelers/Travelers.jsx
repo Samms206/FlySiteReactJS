66fd89673c9241d26e7835e5ac5c094e180c764d
Samms206 <sh2069936@gmail.com>
2024-05-19 15:44:29 +0700

    updated-sectionTraveler

--- src/Components/Travelers/Travelers.jsx
+++ src/Components/Travelers/Travelers.jsx
@@ -1,71 +1,65 @@
-import React from 'react'
+import React from "react";
 
+const travelersData = [
+  {
+    id: 1,
+    destinationImg: "./src/assets/bali.jpeg",
+    travelerImg: "./src/assets/traveler1.jpeg",
+    name: "James Scott",
+    username: "@jamesscott",
+  },
+  {
+    id: 2,
+    destinationImg: "./src/assets/dubai.jpeg",
+    travelerImg: "./src/assets/traveler3.jpeg",
+    name: "Maya Hernandez",
+    username: "@mayahernandez",
+  },
+  {
+    id: 3,
+    destinationImg: "./src/assets/paris.jpeg",
+    travelerImg: "./src/assets/traveler2.jpeg",
+    name: "David Smith",
+    username: "@davidsmith",
+  },
+  {
+    id: 4,
+    destinationImg: "./src/assets/jepang.jpeg",
+    travelerImg: "./src/assets/traveler4.jpeg",
+    name: "Evelyn Kim",
+    username: "@evelynkim",
+  },
+];
 const Travelers = () => {
   return (
-    <div className='travelers container section'>
+    <div className="travelers container section">
       <div className="containerSection">
-        <h2>
-          Top travelers of this month!
-        </h2>
+        <h2>Top travelers of this month!</h2>
 
         <div className="travelersContainer grid">
-
-          <div className="singleTraveler">
-            <img src="./src/assets/bali.jpeg" alt="" className="destinationImg" />
-            <div className="travelerDetails">
-              <div className="travelerPicture">
-                <img src="./src/assets/traveler1.jpeg" alt="" className="travelerImg" />
-              </div>
-              <div className="travelerName">
-                <span>James Scott</span>
-                <p>@jamesscott</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="singleTraveler">
-            <img src="./src/assets/dubai.jpeg" alt="" className="destinationImg" />
-            <div className="travelerDetails">
-              <div className="travelerPicture">
-                <img src="./src/assets/traveler3.jpeg" alt="" className="travelerImg" />
-              </div>
-              <div className="travelerName">
-                <span>James Scott</span>
-                <p>@jamesscott</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="singleTraveler">
-            <img src="./src/assets/jepang.jpeg" alt="" className="destinationImg" />
-            <div className="travelerDetails">
-              <div className="travelerPicture">
-                <img src="./src/assets/traveler2.jpeg" alt="" className="travelerImg" />
-              </div>
-              <div className="travelerName">
-                <span>James Scott</span>
-                <p>@jamesscott</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="singleTraveler">
-            <img src="./src/assets/paris.jpeg" alt="" className="destinationImg" />
-            <div className="travelerDetails">
-              <div className="travelerPicture">
-                <img src="./src/assets/traveler4.jpeg" alt="" className="travelerImg" />
-              </div>
-              <div className="travelerName">
-                <span>James Scott</span>
-                <p>@jamesscott</p>
-              </div>
-            </div>
-          </div>
+          {/* single traveler */}
+          {travelersData.map(
+            ({ id, destinationImg, travelerImg, name, username }) => {
+              return (
+                <div key={id} className="singleTraveler">
+                  <img src={destinationImg} alt="" className="destinationImg" />
+                  <div className="travelerDetails">
+                    <div className="travelerPicture">
+                      <img src={travelerImg} alt="" className="travelerImg" />
+                    </div>
+                    <div className="travelerName">
+                      <span>{name}</span>
+                      <p>{username}</p>
+                    </div>
+                  </div>
+                </div>
+              );
+            }
+          )}
         </div>
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default Travelers
+export default Travelers;
